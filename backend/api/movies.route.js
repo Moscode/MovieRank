90d39f1f7b9301f2383d93acebdfe510332b95ec
Odunosh Moses <Odunoshomoses@gmail.com>
2022-11-27 07:24:51 +0000

    added all routes

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -5,10 +5,12 @@ import ReviewsController from './reviews.controller.js'
 const router =  express.Router() // get access to express router
 
 router.route('/').get(MoviesController.apiGetMovies)
+router.route('/id/:id').get(MoviesController.apiGetMovieById)
+router.route("/rating").get(MoviesController.apiGetRatings)
 
 router
-.route("/review").
-post(ReviewsController.apiPostReview)
+.route("/review")
+.post(ReviewsController.apiPostReview)
 .put(ReviewsController.apiUpdateReview)
 .delete(ReviewsController.apiDeleteReview)
 
