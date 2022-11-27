61daa8d372dd068e1d074ac1d2c4771758e2c061
Odunosh Moses <Odunoshomoses@gmail.com>
2022-11-27 07:25:48 +0000

    building getMovieById and getRatings

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import app from './server.js'
 import mongodb from 'mongodb'
 import dotenv from 'dotenv'
 import MoviesDAO from './dao/movieDAO.js'
+import ReviewsDAO from './dao/reviewsDAO.js'
 
 async function main(){
     dotenv.config()
@@ -15,6 +16,7 @@ async function main(){
         //Connect to the MongoDB cluster
         await client.connect()
         await MoviesDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
         app.listen(port, () => {
             console.log(`Server is running on port: ${port}`)
         })
